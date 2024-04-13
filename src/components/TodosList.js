98bdae985256f447b9a47c830cbff78e695da80a
Sakshi <jaiswalsakshi7890@gmail.com>
2024-04-13 22:26:18 +0530

    update todolist

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -3,18 +3,29 @@ import React from 'react'
 const TodosList = ({todos, setTodos}) => {
   return (
     <div>
-      {todos.map((todo)=>{
-            <li className='todo-list' key={todo.id}>
-                <input type ="text" value ={todo.title}
-                className='list' 
-                onChange={(event) => event.preventDefault} >
-                </input>
-            </li>
+      {todos.map((todo) => (
+    <li className='todo-list' key={todo.id}>
+        <input 
+            type="text" 
+            value={todo.title}
+            className='list' 
+            onChange={(event) => event.preventDefault()} /> {/* Corrected this line */}
+        <div>
+        <button className='button-complete task-button'>
+        <i className='fa fa-check-circle' />
+        </button>
+        <button className='button-edit task-button'>
+        <i className='fa fa-edit' />
+        </button>
+        <button className='button-delete task-button'>
+        <i className='fa fa-trash' />
+        </button>
 
-        })
-      }
-    </div>
-  )
-}
+        </div>
+    </li>
+))}
+</div>
+  );
+};
 
-export default TodosList
+export default TodosList;
